d1b3ae41ec8dff839dc77da873fe6802680d41cc
Igor Zhukov <igor.beatle@gmail.com>
2014-03-12 23:25:08 +0100

    Bump to 0.0.20

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,5 +1,5 @@
 /*!
- * Webogram v0.0.19 - messaging web application for MTProto
+ * Webogram v0.0.20 - messaging web application for MTProto
  * https://github.com/zhukov/webogram
  * Copyright (C) 2014 Igor Zhukov <igor.beatle@gmail.com>
  * https://github.com/zhukov/webogram/blob/master/LICENSE
