376c1173ffd31210111c191704b3eee1380e98d1
Igor Zhukov <igor.beatle@gmail.com>
2014-04-08 13:28:18 +0200

    Added source map while uglifying

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('usemin', ['templates', 'enable-production'], function() {
   return gulp.src('app/index.html')
     .pipe($.usemin({
       html: [$.minifyHtml({empty: true})],
-      js: [$.ngmin(), $.uglify(), 'concat'],
+      js: ['concat', $.ngmin(), $.uglify({outSourceMap: true})],
       css: [$.minifyCss(), 'concat']
     }))
     .pipe(gulp.dest('dist'));
