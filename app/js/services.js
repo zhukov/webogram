59479ea406cf39d13a13843cec719e969298f161
Madh93 <michelangelo93@gmail.com>
2014-10-14 21:31:09 +0100

    Added /?#/ and /#/

--- app/js/services.js
+++ app/js/services.js
@@ -3182,7 +3182,7 @@ angular.module('myApp.services', ['myApp.i18n'])
 
   var regexAlphaNumericChars  = "0-9\.\_" + regexAlphaChars;
   var regExp = new RegExp('((?:(ftp|https?)://|(?:mailto:)?([A-Za-z0-9._%+-]+@))(\\S*\\.\\S*[^\\s.;,(){}<>"\']))|(\\n)|(' + emojiUtf.join('|') + ')|(^|\\s)(#[' + regexAlphaNumericChars + ']{3,20})', 'i');
-  var youtubeRegex = /(?:https?:\/\/)?(?:www\.)?youtu(?:|.be|be.com|.b)(?:\/v\/|\/watch\\?v=|e\/|\/watch(?:.+)v=)(.{11})(?:\&[^\s]*)?/;
+  var youtubeRegex = /(?:https?:\/\/)?(?:www\.)?youtu(?:|.be|be.com|.b)(?:\/v\/|\/watch\\?v=|e\/|(\/(\?#|#))?\/watch(?:.+)v=)(.{11})(?:\&[^\s]*)?/;
 
   return {
     wrapRichText: wrapRichText,
