bd52715f18bf484c0dcb4ca7b40d33ba63983ae7
Igor Zhukov <igor.beatle@gmail.com>
2016-06-09 20:42:00 +0300

    Improved migrate to supergroup check

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3410,13 +3410,13 @@ angular.module('myApp.controllers', ['myApp.i18n'])
       $scope.chatFull = AppChatsManager.wrapForFull($scope.chatID, chatFull);
       $scope.$broadcast('ui_height');
 
-      $scope.canMigrate = $scope.chatFull &&
-                          $scope.chatFull.participants &&
-                          $scope.chatFull.participants.participants &&
-                          $scope.chatFull.participants.participants.length >= 200;
+      $scope.needMigrate = $scope.chatFull &&
+                           $scope.chatFull.participants &&
+                           $scope.chatFull.participants.participants &&
+                           $scope.chatFull.participants.participants.length >= 200;
 
       if (Config.Modes.test || Config.Modes.debug) {
-        $scope.canMigrate = true;
+        $scope.needMigrate = true;
       }
 
       NotificationsManager.savePeerSettings(-$scope.chatID, chatFull.notify_settings);
