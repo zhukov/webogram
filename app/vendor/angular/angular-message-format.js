3a5b82e9992042f21887639839ce0a8eefc46c86
Igor Zhukov <igor.beatle@gmail.com>
2016-06-16 19:39:05 +0300

    Upgrade angular to 1.5.7

--- app/vendor/angular/angular-message-format.js
+++ app/vendor/angular/angular-message-format.js
@@ -1,9 +1,9 @@
 /**
- * @license AngularJS v1.4.7
- * (c) 2010-2015 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.5.7
+ * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+(function(window, angular) {'use strict';
 
 // NOTE: ADVANCED_OPTIMIZATIONS mode.
 //
