98945f3d94cd66c41835f4dff91da7569d94d83a
Serhat Celil İLERİ <celil.ileri@bil.omu.edu.tr>
2016-08-29 23:11:22 +0300

    Turkish Translation Adjustments

--- app/vendor/angular/i18n/angular-locale_tr-tr.js
+++ app/vendor/angular/i18n/angular-locale_tr-tr.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module("ngLocale", [], ["$provide", function($provide) {
-var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
+var PLURAL_CATEGORY = {ZERO: "sıfır", ONE: "bir", TWO: "iki", FEW: "az", MANY: "çok", OTHER: "diğer"};
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
     "AMPMS": [
