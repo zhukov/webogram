9ff88ea2c5cc99dd0c126ca772761d18a60f375f
Igor Zhukov <igor.beatle@gmail.com>
2016-12-16 19:25:09 +0300

    disable unused trace

--- app/js/services.js
+++ app/js/services.js
@@ -1118,7 +1118,7 @@ angular.module('myApp.services', ['myApp.i18n', 'izhukov.utils'])
       if (chatFullPromises[id] !== undefined) {
         return chatFullPromises[id]
       }
-      console.trace(dT(), 'Get chat full', id, AppChatsManager.getChat(id))
+      // console.trace(dT(), 'Get chat full', id, AppChatsManager.getChat(id))
       return chatFullPromises[id] = MtpApiManager.invokeApi('messages.getFullChat', {
         chat_id: AppChatsManager.getChatInput(id)
       }).then(function (result) {
