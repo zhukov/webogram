d7e54db788f720d8ffd83e9d9745414f99003f05
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-04-26 13:20:23 +0200

    improved in-test structure

--- test/unit/filters/timeFilterSpec.js
+++ test/unit/filters/timeFilterSpec.js
@@ -8,59 +8,57 @@ describe('time filter', function () {
     this.$filter = _$filter_
   }))
 
-  it('can create a short time based on timestamp', function () {
-    Config.Mobile = true
+  describe('on the mobile website', function () {
+    beforeEach(function () {
+      Config.Mobile = true
+      this.timeFilter = this.$filter('time')
+    })
 
-    var timeFilter = this.$filter('time')
+    it('can create a short time based on timestamp', function () {
+      var input = 1222
+      var expected = '1:20 AM'
+      var result = this.timeFilter(input)
 
-    var input = 1222
-    var expected = '1:20 AM'
-    var result = timeFilter(input)
+      expect(result).toBe(expected)
+    })
 
-    expect(result).toBe(expected)
+    it('can recollect a short time based on timestamp', function () {
+      var input = 121155
+      var expected = '10:39 AM'
+      var result1 = this.timeFilter(input)
+
+      expect(result1).toBe(expected)
+
+      var result2 = this.timeFilter(input)
+
+      expect(result2).toBe(expected)
+    })
   })
 
-  it('can recollect a short time based on timestamp', function () {
-    Config.Mobile = true
+  describe('on the desktop website', function () {
+    beforeEach(function () {
+      Config.Mobile = false
+      this.timeFilter = this.$filter('time')
+    })
 
-    var timeFilter = this.$filter('time')
+    it('can create a medium-size time based on timestamp', function () {
+      var input = 1222
+      var expected = '1:20:22 AM'
+      var result = this.timeFilter(input)
 
-    var input = 121155
-    var expected = '10:39 AM'
-    var result1 = timeFilter(input)
+      expect(result).toBe(expected)
+    })
 
-    expect(result1).toBe(expected)
+    it('can recollect a medium-size time on timestamp', function () {
+      var input = 121155
+      var expected = '10:39:15 AM'
+      var result1 = this.timeFilter(input)
 
-    var result2 = timeFilter(input)
+      expect(result1).toBe(expected)
 
-    expect(result2).toBe(expected)
-  })
+      var result2 = this.timeFilter(input)
 
-  it('can create a medium-size time based on timestamp', function () {
-    Config.Mobile = false
-
-    var timeFilter = this.$filter('time')
-
-    var input = 1222
-    var expected = '1:20:22 AM'
-    var result = timeFilter(input)
-
-    expect(result).toBe(expected)
-  })
-
-  it('can recollect a medium-size time on timestamp', function () {
-    Config.Mobile = false
-
-    var timeFilter = this.$filter('time')
-
-    var input = 121155
-    var expected = '10:39:15 AM'
-    var result1 = timeFilter(input)
-
-    expect(result1).toBe(expected)
-
-    var result2 = timeFilter(input)
-
-    expect(result2).toBe(expected)
+      expect(result2).toBe(expected)
+    })
   })
 })
