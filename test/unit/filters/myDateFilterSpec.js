55393b8d9192ef8ff8cc26eb24fa63cecdff7cde
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-04-29 12:26:43 +0200

    fixed timezone-problem myDateFilterSpec.js

--- test/unit/filters/myDateFilterSpec.js
+++ test/unit/filters/myDateFilterSpec.js
@@ -10,11 +10,15 @@ describe('myDate filter', function () {
 
   beforeEach(function () {
     this.myDateFilter = this.$filter('myDate')
+
+    // https://stackoverflow.com/questions/4676195/why-do-i-need-to-multiply-unix-timestamps-by-1000-in-javascript
+    this.miliSecondsToSeconds = 1000
+    this.dateFilter = this.$filter('date')
   })
 
   it('can create a date based on timestamp', function () {
     var input = 1222222222
-    var expected = 'Wednesday, September 24, 2008'
+    var expected = this.dateFilter(input * this.miliSecondsToSeconds, 'fullDate') // For CEST: 'Wednesday, September 24, 2008'
     var result = this.myDateFilter(input)
 
     expect(result).toBe(expected)
@@ -22,7 +26,7 @@ describe('myDate filter', function () {
 
   it('can recollect a date based on timestamp', function () {
     var input = 12111114111
-    var expected = 'Thursday, October 15, 2353'
+    var expected = this.dateFilter(input * this.miliSecondsToSeconds, 'fullDate') // For CEST: 'Thursday, October 15, 2353'
     var result1 = this.myDateFilter(input)
 
     expect(result1).toBe(expected)
