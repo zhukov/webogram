dbf17307b207b20bcbc4d6d84e9042b2b621cd61
boivlad <boyvlad99@gmail.com>
2020-11-24 16:34:33 +0200

    completed balance page

--- app/js/lib/incognito.js
+++ app/js/lib/incognito.js
@@ -2,7 +2,9 @@ angular.module('incognito', [])
 
   .factory('InAPIConfigurator', function() {
     return {
-      chooseServer: function () {return ('http://localhost:8080')}
+      chooseServer: function() {
+        return ('http://localhost:8080')
+      }
     }
   })
 
@@ -11,21 +13,50 @@ angular.module('incognito', [])
     delete $http.defaults.headers.common['Accept']
 
     function getAccountData() {
-      var url = InAPIConfigurator.chooseServer();
+      // var url = InAPIConfigurator.chooseServer();
       try {
         // requestPromise = $http.get(url);
-        var requestPromise = new Promise(function(resolve, reject)  {
+        var requestPromise = new Promise(function(resolve, reject) {
           resolve(
             {
               data: {
-                accounts : {
-                  "Anon" : {
-                    balance: 2,
+                accounts: {
+                  Anon: {
+                    balance: 0,
                     address: "12S3CcAtftrYJn1woQVUx94RdcTTRXoSyFM76wjyTesf6AvhMPw94u16TUJCdubsQMuu7J9L4pzukxY1DxqVFv91Y72b3LoDgRKKknJ",
                     privateKey: "112t8rnXB9Tej38VBMAYMQGZz1KUaKYbhqDQpdBwga2aGF92GNULFW8buLFidMvxZJvQvDVyfZCU8T8KJ6vb7sRDCbJg81QeoJJC3EgLuvfT",
                     publicKey: "12WHkqsGtbZ66xS8iawqruhN7DyHv6jgBAmEb9PiyrAsiehei4E",
                     readOnlyKey: "13henmYoRgvwKVgRRUfqnq3MZLVedYny5TYvMvQZYbzu1Ys6Y8MBEaE4GmyJwQnZQ5j1fHTbSLMrFz2vV7TcRUvNphk8ZY6vWnDbCT6",
-                    validatorKey: "1kitnhgpfRyg8VHLWxb2jrirYm1gnM1sr8AK9fc2zoHJd8vPYm"
+                    validatorKey: "1kitnhgpfRyg8VHLWxb2jrirYm1gnM1sr8AK9fc2zoHJd8vPYm",
+                    wallets: [
+                      {
+                        name: "Privacy",
+                        symbol: "RPV",
+                        balance: 0,
+                        verified: true
+                      },
+                      {
+                        name: "Ethereum",
+                        symbol: "ETH",
+                        balance: 0,
+                        price: 356.2051,
+                        verified: true
+                      },
+                      {
+                        name: "Bitcoin",
+                        symbol: "BTC",
+                        balance: 0,
+                        price: 10609.6427,
+                        verified: true
+                      },
+                      {
+                        name: "Tether USD",
+                        symbol: "USDT",
+                        balance: 0,
+                        price: 0.581684746,
+                        verified: false
+                      }
+                    ]
                   }
                 }
               }
