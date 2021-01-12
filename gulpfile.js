01f009662c5af9884cf8e6918f999d1e84298acb
Hayden Blaise <haydenlueck@gmail.com>
2021-01-11 20:53:12 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('copy-images', function () {
 
 gulp.task('copy', gulp.parallel(
   function () {
-    return gulp.src(['app/favicon.ico', 'app/favicon_unread.ico', 'app/manifest.webapp', 'app/manifest.webapp.json', 'app/manifest.json', 'app/**/*worker.js'])
+    return gulp.src(['app/favicon.ico', 'app/img/unread_favicons/favicon_unread_1.ico', 'app/img/unread_favicons/favicon_unread_2.ico', 'app/img/unread_favicons/favicon_unread_3.ico', 'app/img/unread_favicons/favicon_unread_4.ico', 'app/img/unread_favicons/favicon_unread_5.ico', 'app/img/unread_favicons/favicon_unread_6.ico', 'app/img/unread_favicons/favicon_unread_7.ico', 'app/img/unread_favicons/favicon_unread_8.ico', 'app/img/unread_favicons/favicon_unread_9.ico', 'app/img/unread_favicons/favicon_unread_9_plus.ico', 'app/manifest.webapp', 'app/manifest.webapp.json', 'app/manifest.json', 'app/**/*worker.js'])
       .pipe(gulp.dest('dist'))
   },
   function () {
@@ -263,7 +263,7 @@ gulp.task('package-dev', gulp.parallel(
       .pipe(gulp.dest('dist_package/js'));
   },
   function () {
-    return gulp.src(['app/favicon.ico', 'app/favicon_unread.ico', 'app/manifest.webapp', 'app/manifest.json'])
+    return gulp.src(['app/favicon.ico', 'app/img/unread_favicons/favicon_unread_1.ico', 'app/img/unread_favicons/favicon_unread_2.ico', 'app/img/unread_favicons/favicon_unread_3.ico', 'app/img/unread_favicons/favicon_unread_4.ico', 'app/img/unread_favicons/favicon_unread_5.ico', 'app/img/unread_favicons/favicon_unread_6.ico', 'app/img/unread_favicons/favicon_unread_7.ico', 'app/img/unread_favicons/favicon_unread_8.ico', 'app/img/unread_favicons/favicon_unread_9.ico', 'app/img/unread_favicons/favicon_unread_9_plus.ico', 'app/manifest.webapp', 'app/manifest.json'])
       .pipe(gulp.dest('dist_package'));
   },
   function () {
