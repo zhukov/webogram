e1b581455b0e03e3246272a4b3738db5208018bc
Igor Zhukov <igor@zhukov.io>
2023-05-06 17:45:31 +0200

    New worker

--- service_worker.js
+++ service_worker.js
@@ -1,1 +1,7 @@
-// Disabled
+self.addEventListener('install', (e) => {
+    e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
+});
